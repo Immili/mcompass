cebded8ce9a2a420a8b2b917a20c5f29951845c7
Immili <100063037+Immili@users.noreply.github.com>
2025-08-05 22:54:58 +0800

    Update sensor_impl.cpp

--- Firmware/src/impl/sensor_impl.cpp
+++ Firmware/src/impl/sensor_impl.cpp
@@ -133,6 +133,9 @@ int sensor::getAzimuth() {
   case SensorModel::QMC5883L: {
     azimuth = 360 - azimuth; // 将方位角转换为0-360度范围
   }
+    case SensorModel::MMC5883MA: {
+    azimuth = 360 - azimuth; // 将方位角转换为0-360度范围
+  }
   default:
     break;
   }
